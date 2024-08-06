06fbff9c644ed0397c2f4a3eb3f90ccd4474cbc8
HirokoEguchi <uni202406_02@telework.to>
2024-08-06 14:14:49 +0900

    画像をDictionary型にしクラスで管理

--- RandomNumberGenerator/Form1.cs
+++ RandomNumberGenerator/Form1.cs
@@ -4,9 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RandomNumberGenerator
 {
@@ -16,43 +18,57 @@ namespace RandomNumberGenerator
         //フィールドとしてRandomオブジェクトを宣言
         private Random random;
         private Button button1;
-
+        /*
         private Bitmap image1;
         private Bitmap image2;
         private Bitmap image3;
         private Bitmap image4;
         private Bitmap image5;
         private Bitmap image6;
+        */
 
-        private Point image1Position;
-        private Point image2Position;
-        private Point image3Position;
+        private PictureBox picturebox1;
+        private PictureBox picturebox2;
+        private PictureBox picturebox3;
 
+       
 
         //Formの初期化
         public Form1()
         {
             InitializeComponent();
 
-            image1 = new Bitmap("saikoro_1.png");
-            image2 = new Bitmap("saikoro_2.png");
-            image3 = new Bitmap("saikoro_3.png");
-            image4 = new Bitmap("saikoro_4.png");
-            image5 = new Bitmap("saikoro_5.png");
-            image6 = new Bitmap("saikoro_6.png");
+
+            
+
 
             // 初期位置の設定
-            image1Position = new Point(50, 70);
-            image2Position = new Point(150, 70);
-            image3Position = new Point(250, 70);
+            picturebox1 = new PictureBox
+            {
+               Location =new Point(50, 70),
+               Size =new Size(60,60),
+               Name = "picturebox1",
+               Image = null
+            };
 
-            // ダブルバッファリングを有効にする
-            this.DoubleBuffered = true;
+            picturebox2 = new PictureBox
+            {
+                Location = new Point(150, 70),
+                Size = new Size(60, 60),
+                Name = "picturebox1",
+                Image = null
+            };
+
+            picturebox3 = new PictureBox
+            {
+                Location = new Point(250, 70),
+                Size = new Size(60, 60),
+                Name = "picturebox1",
+                Image = null
+            };
 
             //Randomオブジェクトを初期化
             random = new Random();
-
-
 
 
             //Labelをコードで生成
@@ -68,9 +84,6 @@ namespace RandomNumberGenerator
             //Labelをフォームに追加
             this.Controls.Add(label1);
             */
-
-
-
 
 
             //Buttonをコードで生成
@@ -89,10 +102,13 @@ namespace RandomNumberGenerator
             this.Controls.Add(button1);
         }
 
+        //画像の初期位置・サイズ
+        /*
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
             Graphics g = e.Graphics;
+
 
             // 画像の描画
             if (image1 != null)
@@ -111,22 +127,53 @@ namespace RandomNumberGenerator
             }
 
         }
+        */
 
 
 
-        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+        List<int> numbers1 = new List<int> { 1, 2, 3, 4, 5, 6 };
+        List<int> numbers2 = new List<int> { 1, 2, 3, 4, 5, 6 };
+        List<int> numbers3 = new List<int> { 1, 2, 3, 4, 5, 6 };
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // ランダムな数字を生成する
 
             // ランダムに要素番号を選択
-            int randomIndex = random.Next(numbers.Count); // 0からnumbers.Count - 1までのランダムな整数を生成
+            int randomIndex = random.Next(numbers1.Count); // 0からnumbers.Count - 1までのランダムな整数を生成
 
             // ランダムに選ばれた要素を表示
-            int selectedNumber = numbers[randomIndex];
+            int selectedNumber = numbers1[randomIndex];
+
         }
     }
 
+    public class Example : Form
+    {
+        private Dictionary<int, Bitmap> images;
+
+        public Example()
+        {
+            // 画像をロードして Dictionary に追加
+            images = new Dictionary<int, Bitmap>();
+            images[1] = new Bitmap("saikoro_1.png");
+            images[2] = new Bitmap("saikoro_2.png");
+            images[3] = new Bitmap("saikoro_3.png");
+            images[4] = new Bitmap("saikoro_4.png");
+            images[5] = new Bitmap("saikoro_5.png");
+            images[5] = new Bitmap("saikoro_6.png");
+
+            // フォームの初期設定
+            this.Text = "Image Dictionary Example";
+            this.Size = new Size(60, 60);
+
+            // ダブルバッファリングを有効にする
+            this.DoubleBuffered = true;
+
+        }
+
+
+
+    }
 
 }
